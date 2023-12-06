cb0000fba84e471e9a2ef980bce2952b531d9ab2
sky04281 <119783141+sky04281@users.noreply.github.com>
2023-12-06 22:33:52 +0800

    1206
    PatientList 修好了 可以接值

--- client/src/pages/PatientList.tsx
+++ client/src/pages/PatientList.tsx
@@ -128,154 +128,157 @@ function ForDateChoose() {
 }
 
 function PatientList() {
-    const [patient, setPatient] = useState<Patient>({
-        info: {
-            "ID#": "",
-            name: "",
-            DOB: "2023-11-29",
-            sex: "",
-            age: 0,
-            height: 0,
-            weight: 0,
-            status: "候診",
-            other: "unknown",
-            attackDate: "2023-11-29",
-            beginSymptom: "beginSymptom",
-            otherHospitalRecord: {
-                recentlyDate: "2023-11-29",
-                totalTimes: 0,
-            },
-            otherDisease: ["diseaseOne"],
-            otherMedicine: ["medicineOne"],
-        },
-        thymus: [
-            {
-                number: 0,
-                testDate: "2023-11-29",
-                thymusStatus: 0,
-                thymusDescription: "thymusDescription",
-            },
-        ],
-        bloodTest: [
-            {
-                number: 0,
-                testDate: "2023-11-29",
-                ACHR: 0,
-                TSH: 0,
-                freeThyroxine: 0,
-                ANA: 0,
-                uricAcid: 0,
-            },
-        ],
-        QOLtable: [
-            {
-                number: 0,
-                testDate: "2023-11-29",
-                frustration: 0,
-                eyeUsing: 0,
-                eating: 0,
-                social: 0,
-                entertainment: 0,
-                fullfillFamilyNeeds: 0,
-                plansNecessity: 0,
-                jobState: 0,
-                speaking: 0,
-                driving: 0,
-                depression: 0,
-                walking: 0,
-                beingInPublicPlaces: 0,
-                overwhelm: 0,
-                freshenUp: 0,
-                sum: 0,
-            },
-        ],
-        QMGtable: [
-            {
-                number: 0,
-                testDate: "2023-11-29",
-                doubleVision: 0,
-                ptosis: 0,
-                facialMuscle: 0,
-                swallowing: 0,
-                speakFluency: 0,
-                rightArmHeight: 0,
-                leftArmHeight: 0,
-                vitalCapacity: 0,
-                rightHandGrid: 0,
-                leftHandGrid: 0,
-                headLift: 0,
-                rightLegHeight: 0,
-                leftLegHeight: 0,
-                sum: 0,
-            },
-        ],
-        MGtable: [
-            {
-                number: 0,
-                testDate: "2023-11-29",
-                ptosis: 0,
-                doubleVision: 0,
-                eyeClosure: 0,
-                talking: 0,
-                chewing: 0,
-                swallowing: 0,
-                breathing: 0,
-                neckFlexion: 0,
-                shoulderAbduction: 0,
-                hipFlexion: 0,
-                sum: 0,
-            },
-        ],
-        ADLtable: [
-            {
-                number: 0,
-                testDate: "2023-11-29",
-                talking: 0,
-                chewing: 0,
-                swallowing: 0,
-                breathing: 0,
-                brushTeethOrCombHair: 0,
-                ariseFromChair: 0,
-                eyelid: 0,
-                sum: 0,
-            },
-        ],
-        visit: [
-            {
-                number: 0,
-                date: "2023-11-29",
-                treat: 0,
-                SBP: 0,
-                DBP: 0,
-                examination: {
-                    ptosis: 0,
-                    diplopia: 0,
-                    dysphagia: 0,
-                    dysarthria: 0,
-                    dyspnea: 0,
-                    limpWeakness: 0,
-                    MGFAclassification: 0,
-                },
-                Prescription: {
-                    pyridostigmine: 0,
-                    compesolone: 0,
-                    cellcept: 0,
-                    imuran: 0,
-                    prograf: 0,
-                },
-                selfAssessment: 0,
-                note: "note",
-            },
-        ],
-    });
+    const [patients, setPatients] = useState<Patient[]>([
+    // 不用初始值也可以跑 看妳們要不要留著
+    // {
+    //     info: {
+    //         "ID#": "",
+    //         name: "",
+    //         DOB: "2023-11-29",
+    //         sex: "",
+    //         age: 0,
+    //         height: 0,
+    //         weight: 0,
+    //         status: "候診",
+    //         other: "unknown",
+    //         attackDate: "2023-11-29",
+    //         beginSymptom: "beginSymptom",
+    //         otherHospitalRecord: {
+    //             recentlyDate: "2023-11-29",
+    //             totalTimes: 0,
+    //         },
+    //         otherDisease: ["diseaseOne"],
+    //         otherMedicine: ["medicineOne"],
+    //     },
+    //     thymus: [
+    //         {
+    //             number: 0,
+    //             testDate: "2023-11-29",
+    //             thymusStatus: 0,
+    //             thymusDescription: "thymusDescription",
+    //         },
+    //     ],
+    //     bloodTest: [
+    //         {
+    //             number: 0,
+    //             testDate: "2023-11-29",
+    //             ACHR: 0,
+    //             TSH: 0,
+    //             freeThyroxine: 0,
+    //             ANA: 0,
+    //             uricAcid: 0,
+    //         },
+    //     ],
+    //     QOL: [
+    //         {
+    //             number: 0,
+    //             testDate: "2023-11-29",
+    //             frustration: 0,
+    //             eyeUsing: 0,
+    //             eating: 0,
+    //             social: 0,
+    //             entertainment: 0,
+    //             fullfillFamilyNeeds: 0,
+    //             plansNecessity: 0,
+    //             jobState: 0,
+    //             speaking: 0,
+    //             driving: 0,
+    //             depression: 0,
+    //             walking: 0,
+    //             beingInPublicPlaces: 0,
+    //             overwhelm: 0,
+    //             freshenUp: 0,
+    //             sum: 0,
+    //         },
+    //     ],
+    //     QMG: [
+    //         {
+    //             number: 0,
+    //             testDate: "2023-11-29",
+    //             doubleVision: 0,
+    //             ptosis: 0,
+    //             facialMuscle: 0,
+    //             swallowing: 0,
+    //             speakFluency: 0,
+    //             rightArmHeight: 0,
+    //             leftArmHeight: 0,
+    //             vitalCapacity: 0,
+    //             rightHandGrid: 0,
+    //             leftHandGrid: 0,
+    //             headLift: 0,
+    //             rightLegHeight: 0,
+    //             leftLegHeight: 0,
+    //             sum: 0,
+    //         },
+    //     ],
+    //     MG: [
+    //         {
+    //             number: 0,
+    //             testDate: "2023-11-29",
+    //             ptosis: 0,
+    //             doubleVision: 0,
+    //             eyeClosure: 0,
+    //             talking: 0,
+    //             chewing: 0,
+    //             swallowing: 0,
+    //             breathing: 0,
+    //             neckFlexion: 0,
+    //             shoulderAbduction: 0,
+    //             hipFlexion: 0,
+    //             sum: 0,
+    //         },
+    //     ],
+    //     ADL: [
+    //         {
+    //             number: 0,
+    //             testDate: "2023-11-29",
+    //             talking: 0,
+    //             chewing: 0,
+    //             swallowing: 0,
+    //             breathing: 0,
+    //             brushTeethOrCombHair: 0,
+    //             ariseFromChair: 0,
+    //             eyelid: 0,
+    //             sum: 0,
+    //         },
+    //     ],
+    //     visit: [
+    //         {
+    //             number: 0,
+    //             date: "2023-11-29",
+    //             treat: 0,
+    //             SBP: 0,
+    //             DBP: 0,
+    //             examination: {
+    //                 ptosis: 0,
+    //                 diplopia: 0,
+    //                 dysphagia: 0,
+    //                 dysarthria: 0,
+    //                 dyspnea: 0,
+    //                 limpWeakness: 0,
+    //                 MGFAclassification: 0,
+    //             },
+    //             Prescription: {
+    //                 pyridostigmine: 0,
+    //                 compesolone: 0,
+    //                 cellcept: 0,
+    //                 imuran: 0,
+    //                 prograf: 0,
+    //             },
+    //             selfAssessment: 0,
+    //             note: "note",
+    //         },
+    //     ],
+    // }
+]);
 
     const data = async () => {
         const response = await api.get("/readAllPatient");
-        setPatient(response.data);
+        setPatients(response.data);
     };
 
-    console.log(patient);
-    Object.values(patient).map((patient) => console.log(patient["info"]));
+    console.log(patients);
+    patients.map((patient) => console.log(patient["info"]));
 
     useEffect(() => {
         data();
@@ -390,7 +393,7 @@ function PatientList() {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {Object.values(patient).map((patient, index) => (
+                            {patients.map((patient, index) => (
                                 <TableRow key={index} hover={true}>
                                     <TableCell
                                         component="th"
@@ -418,6 +421,9 @@ function PatientList() {
                                         </ButtonBase>
                                     </TableCell>
                                     <TableCell align="center">
+                                        {patient["info"]["status"]}
+                                    </TableCell>
+                                    <TableCell align="center">
                                         {patient["info"]["name"]}
                                     </TableCell>
                                     <TableCell align="center">
@@ -428,7 +434,7 @@ function PatientList() {
                                     </TableCell>
 
                                     <TableCell align="center">
-                                        {patient["info"]["id"]}
+                                        {patient["info"]["ID#"]}
                                     </TableCell>
                                     <TableCell
                                         align="left"
