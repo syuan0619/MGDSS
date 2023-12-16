5ccd5efba88cb3ff3b81d092b8fd0941e5457019
iamthe60 <harrisonlu5888@gmail.com>
2023-12-16 17:18:34 +0800

    1216

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,20 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import PatientList from "./pages/PatientList";
-<<<<<<< Updated upstream
-import "./style/App.css"
-
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<LoginPage />}></Route>
-                <Route path="/register" element={<RegisterPage />}></Route>
-                <Route path="/patient" element={<PatientList />}></Route>
-            </Routes>
-        </BrowserRouter>
-    );
-=======
 import OCRPage from "./pages/OCRPage";
 import Records from "./pages/Records";
 
@@ -34,7 +20,6 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
->>>>>>> Stashed changes
 }
 
 export default App;
