e3762cf4e391c0cf8d707f51b9cff4f9bb648a2f
iamthe60 <harrisonlu5888@gmail.com>
2023-12-21 03:09:21 +0800

    小改

--- client/src/pages/Records.tsx
+++ client/src/pages/Records.tsx
@@ -5,47 +5,52 @@ import {
   Paper,
   styled,
   Tab,
+  TableCell,
+  TableRow,
   Tabs,
   TextField,
   Typography,
 } from "@mui/material";
 import * as React from "react";
 import "../style/Records.css";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import { ArrowBackRounded } from "@mui/icons-material";
+import Patient from "../types/Schema";
+import api from "../api";
+import { useEffect, useState } from "react";
 
-function CustomTabPanel(props) {
-  const { children, value, index, ...other } = props;
+// function CustomTabPanel(props) {
+//   const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`simple-tabpanel-${index}`}
+//       aria-labelledby={`simple-tab-${index}`}
+//       {...other}
+//     >
+//       {value === index && (
+//         <Box sx={{ p: 3 }}>
+//           <Typography>{children}</Typography>
+//         </Box>
+//       )}
+//     </div>
+//   );
+// }
 
-CustomTabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
+// CustomTabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.number.isRequired,
+//   value: PropTypes.number.isRequired,
+// };
 
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
+// function a11yProps(index) {
+//   return {
+//     id: `simple-tab-${index}`,
+//     "aria-controls": `simple-tabpanel-${index}`,
+//   };
+// }
 
 const Records = () => {
   const [value, setValue] = React.useState(0);
@@ -53,11 +58,24 @@ const Records = () => {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const [patients, setPatients] = useState<Patient[]>([]);
+  const data = async () => {
+    const response = await api.get("/readAllPatient");
+    setPatients(response.data);
+  };
+
+  console.log(patients);
+  patients.map((patient) => console.log(patient["info"]));
+
+  useEffect(() => {
+    data();
+  }, []);
   return (
     <>
       <div className="container">
         <div className="top">
-          <div className="context">
+          <div className="topcontext">
             <div className="topinnercontext">
               <ArrowBackRounded
                 sx={{ fontSize: "3rem", color: "#0080FF" }}
@@ -78,133 +96,127 @@ const Records = () => {
           <div className="context">
             <div className="innercontext">
               <h1>病患資料</h1>
-              <p />
-              <p>
-                <TextField
-                  label="ID:"
-                  id="ID#"
-                  defaultValue="#ID"
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="姓名:"
-                  id="name"
-                  defaultValue="name"
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  id="DOB"
-                  defaultValue="2000/01/01"
-                  size="small"
-                  label="生日:"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="性別:"
-                  id="sex"
-                  defaultValue=" "
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="年齡:"
-                  id="age"
-                  defaultValue="10"
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="身高:"
-                  id="height"
-                  defaultValue="170"
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="體重:"
-                  id="weight"
-                  defaultValue="50"
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="狀態:"
-                  id="status"
-                  defaultValue=""
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="其他:"
-                  id="other"
-                  defaultValue=""
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="初診日期:"
-                  id="attackDate"
-                  defaultValue="2020/01/01"
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="初始症狀:"
-                  id="beginSymptom"
-                  defaultValue=""
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="其他醫院病歷:"
-                  id="otherHospitalRecord"
-                  defaultValue=""
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="其他疾病:"
-                  id="otherDisease"
-                  defaultValue=""
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
-              <p>
-                <TextField
-                  label="其他藥物:"
-                  id="otherMedicine"
-                  defaultValue=""
-                  size="small"
-                  sx={{ width: "10rem" }}
-                />
-              </p>
+              {patients.map((patient) => (
+                <>
+                  <p>
+                    ID:
+                    <TextField
+                      InputProps={{
+                        readOnly: true,
+                      }}
+                      defaultValue={patient["info"]["ID#"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+
+                  <p>
+                    姓名:
+                    <TextField
+                      defaultValue={patient["info"]["name"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    生日:
+                    <TextField
+                      defaultValue={patient["info"]["DOB"]}
+                      size="small"
+                      label="生日:"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    性別:
+                    <TextField
+                      defaultValue={patient["info"]["sex"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    年齡:
+                    <TextField
+                      defaultValue={patient["info"]["age"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    身高:
+                    <TextField
+                      defaultValue={patient["info"]["height"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    體重:
+                    <TextField
+                      defaultValue={patient["info"]["weight"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    狀態:
+                    <TextField
+                      defaultValue={patient["info"]["status"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    其他:
+                    <TextField
+                      defaultValue={patient["info"]["other"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    初診日期:
+                    <TextField
+                      defaultValue={patient["info"]["attackDate"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    初始症狀:
+                    <TextField
+                      defaultValue={patient["info"]["beginSymptom"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    其他醫院病歷:
+                    <TextField
+                      defaultValue={patient["info"]["otherHospitalRecord"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    其他疾病:
+                    <TextField
+                      defaultValue={patient["info"]["otherDisease"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                  <p>
+                    其他藥物:
+                    <TextField
+                      defaultValue={patient["info"]["otherMedicine"]}
+                      size="small"
+                      sx={{ width: "10rem" }}
+                    />
+                  </p>
+                </>
+              ))}
             </div>
           </div>
         </div>
@@ -218,43 +230,29 @@ const Records = () => {
               <h1>Bloodtest</h1>
               <TextField
                 label="測試日期:"
-                id="testDate"
                 defaultValue=""
                 sx={{ width: "20rem" }}
               />
               <p />
               <TextField
                 label="ACHR:"
-                id="ACHR"
                 defaultValue=""
                 sx={{ width: "20rem" }}
               />
               <p />
-              <TextField
-                label="TSH:"
-                id="TSH"
-                defaultValue=""
-                sx={{ width: "20rem" }}
-              />
+              <TextField label="TSH:" defaultValue="" sx={{ width: "20rem" }} />
               <p />
               <TextField
                 label="Free Thyroxine:"
-                id="freeThyroxine"
                 defaultValue=""
                 sx={{ width: "20rem" }}
               />
               <p />
-              <TextField
-                label="ANA:"
-                id="ANA"
-                defaultValue=""
-                sx={{ width: "20rem" }}
-              />
+              <TextField label="ANA:" defaultValue="" sx={{ width: "20rem" }} />
               <p />
 
               <TextField
                 label="UricAcid:"
-                id="uricAcid"
                 defaultValue=""
                 sx={{ width: "20rem" }}
               />
