ef04f389d7c2e0877d7baea338669c04ffe1f2c3
sky04281 <119783141+sky04281@users.noreply.github.com>
2023-12-20 09:44:15 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import PatientList from "./pages/PatientList";
-<<<<<<< Updated upstream
-import "./style/App.css"
+import OCRPage from "./pages/OCRPage";
+import Records from "./pages/Records";
+
+import "./style/App.css";
 
 function App() {
     return (
@@ -13,28 +15,11 @@ function App() {
                 <Route path="/" element={<LoginPage />}></Route>
                 <Route path="/register" element={<RegisterPage />}></Route>
                 <Route path="/patient" element={<PatientList />}></Route>
+                <Route path="/OCR" element={<OCRPage />}></Route>
+                <Route path="/records" element={<Records />}></Route>
             </Routes>
         </BrowserRouter>
     );
-=======
-import OCRPage from "./pages/OCRPage";
-import Records from "./pages/Records";
-
-import "./style/App.css";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />}></Route>
-        <Route path="/register" element={<RegisterPage />}></Route>
-        <Route path="/patient" element={<PatientList />}></Route>
-        <Route path="/OCR" element={<OCRPage />}></Route>
-        <Route path="/records" element={<Records />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
->>>>>>> Stashed changes
 }
 
 export default App;
