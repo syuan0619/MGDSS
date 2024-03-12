be76e5f901583a1c55548dc074eb7819d02b6372
iamthe60 <harrisonlu5888@gmail.com>
2024-03-13 00:13:41 +0800

    救資料：（

--- client/src/pages/inquiryPage/table/QOL.tsx
+++ client/src/pages/inquiryPage/table/QOL.tsx
@@ -1,40 +1,60 @@
-import { Slider, styled } from "@mui/material";
+import { Slider } from "@mui/material";
+import { number } from "prop-types";
+import * as React from "react";
+import { useState } from "react";
 import "./QOL.css";
-const PrettoSlider = styled(Slider)({
-  "& .MuiSlider-track": {
-    border: "none",
+const marks = [
+  {
+    value: 0,
+    label: "0",
   },
-  "& .MuiSlider-thumb": {
-    "&::before": {
-      display: "none",
-    },
+  {
+    value: 1,
+    label: "1",
   },
-  "& .MuiSlider-valueLabel": {
-    lineHeight: 1.2,
-    fontSize: 12,
-    background: "unset",
-    padding: 0,
-    width: 32,
-    height: 32,
-    borderRadius: "50% 50% 50% 0",
-    backgroundColor: "#52af77",
-    transformOrigin: "bottom left",
-    transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
-    "&::before": { display: "none" },
-    "&.MuiSlider-valueLabelOpen": {
-      transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
-    },
-    "& > *": {
-      transform: "rotate(45deg)",
-    },
+  {
+    value: 2,
+    label: "2",
   },
-});
+];
 
 const QOL = ({
   setReplaceComponent,
 }: {
   setReplaceComponent: (table: string) => void;
 }) => {
+  const [qolScore, setQolScore] = useState({
+    frustration: 0,
+    eyeUsing: 0,
+    eating: 0,
+    social: 0,
+    entertainment: 0,
+    fullfillFamilyNeeds: 0,
+    plansNecessity: 0,
+    jobState: 0,
+    speaking: 0,
+    driving: 0,
+    depression: 0,
+    walking: 0,
+    beingInPublicPlaces: 0,
+    overwhelm: 0,
+    freshenUp: 0,
+    sum: 0,
+  });
+
+  const scoreQolInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQolScore({ ...qolScore, [e.target.name]: e.target.value });
+  };
+
+  let qolSum = 0;
+  Object.values(qolScore)
+    .slice(0, -1)
+    .map((item) => (qolSum += item));
+  qolScore["sum"] = qolSum;
+
+  const onSubmitQolScore = () => {
+    console.log(qolScore);
+  };
   return (
     <>
       <div className="inquiry-table-QOL">
@@ -43,13 +63,167 @@ const QOL = ({
         </button>
         <div className="inquiry-table-QOL-header">QOL</div>
         <div className="inquiry-table-QOL-content">
-          <div className="inquiry-table-QOL-content-sliderbox">
-            <PrettoSlider
-              valueLabelDisplay="auto"
-              defaultValue={0}
-              className="inquiry-table-QOL-content-slider"
-              max={2}
-            />
+          <div className="inquiry-table-QOL-content-block">
+            <div className="inquiry-table-QOL-content-sliderbox">
+              <input
+                className="inquiry-table-QOL-content-block-textfield"
+                type="date"
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Frustration
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+                onChange={scoreQolInput}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Eye Using
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Eating
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Social
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Entertainment
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+          </div>
+          <div className="inquiry-table-QOL-content-block">
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Full Fill Family Needs
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Plans Necessity
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Job State
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Speaking
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Driving
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              <input
+                className="inquiry-table-QOL-content-block-textfield"
+                disabled
+              />
+            </div>
+          </div>
+          <div className="inquiry-table-QOL-content-block">
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Depression
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Walking
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Being In Public Places
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Overwhelm
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              Freshen Up
+              <Slider
+                valueLabelDisplay="auto"
+                defaultValue={0}
+                max={2}
+                marks={marks}
+              />
+            </div>
+            <div className="inquiry-table-QOL-content-sliderbox">
+              <button
+                className="inquiry-table-QOL-content-block-buttom"
+                onClick={onSubmitQolScore}
+              >
+                確定
+              </button>
+            </div>
           </div>
         </div>
       </div>
