d45890c2a0fc9b46b9245e7f2fd9ca5844e96738
iamthe60 <harrisonlu5888@gmail.com>
2024-04-06 01:59:18 +0800

    EMG可以選日期看當天的post

--- client/src/pages/inquiryPage/chart/EMG.tsx
+++ client/src/pages/inquiryPage/chart/EMG.tsx
@@ -3,6 +3,8 @@ import { IoIosArrowDropleftCircle } from "react-icons/io";
 import "./Chart.css";
 import { EMG } from "../../../types/Patient";
 import * as React from "react";
+import { Tab, Tabs } from "@mui/material";
+import { useState } from "react";
 const patientEMG = [
   {
     testDate: "2024-04-13",
@@ -22,17 +24,36 @@ const patientEMG = [
     ],
   },
 ];
+//pre and post data
 const postXLabel = [1, 2, 3, 4].map((x) => x);
 const EMGxLabels = patientEMG.map((item) => item.testDate);
 const firstPreData = patientEMG.map((item) => item.RNS[0][1] as number);
-const firstPostData = patientEMG[0]["RNS"][0]
-  .slice(2)
-  .map((item) => item as unknown as number);
-const firstName = patientEMG.map((item) => item.RNS[0][0]);
-
+const firstName = patientEMG.map((item) => item.RNS[0][0] as string);
 const secPreData = patientEMG.map((item) => item.RNS[1][1] as number);
-const secPostData1 = patientEMG.map((item) => item.RNS[1][2] as number);
+const secPostData = patientEMG.map((item) => item.RNS[1][2] as number);
 const secName = patientEMG.map((item) => item.RNS[1][0]);
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div role="tabpanel" hidden={value !== index} {...other}>
+      {value === index && <div>{children}</div>}
+    </div>
+  );
+}
 
 const EMGChart = ({
   setReplaceComponent,
@@ -43,11 +64,30 @@ const EMGChart = ({
 }) => {
   console.log(historyData);
 
+  //tab
   const [value, setValue] = React.useState(0);
-
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
+
+  //////date
+  const dateOptions = patientEMG.map((item) => (
+    <option value={item.testDate}>{item.testDate}</option>
+  ));
+  const [selectedOption, setSelectedOption] = useState("");
+  const ChangeDate = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setSelectedOption(event.target.value);
+  };
+
+  //select date
+  const tarDate = selectedOption;
+  const tarData = patientEMG.filter((item) => item.testDate === tarDate);
+  const firstPostData =
+    tarData[0]?.["RNS"]?.[0]
+      ?.slice(2)
+      ?.map((item) => item as unknown as number) ?? [];
 
   return (
     <div className="chart-bg">
@@ -61,27 +101,90 @@ const EMGChart = ({
           </button>
           <p>EMG</p>
         </div>
-        <div className="chart-footer">
-          <LineChart
-            margin={{ top: 100 }}
-            slotProps={{
-              legend: {
-                itemGap: 20,
-                padding: -5,
-                itemMarkHeight: 5,
-              },
+        <div className="chart-footer" style={{ flexDirection: "column" }}>
+          <div
+            style={{
+              height: "100%",
+              margin: "3vh",
             }}
-            width={700}
-            height={500}
-            series={[
-              {
-                curve: "linear",
-                data: firstPostData,
-                label: "post Activation",
-              },
-            ]}
-            xAxis={[{ scaleType: "point", data: postXLabel }]}
-          />
+          >
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="basic tabs example"
+            >
+              <Tab label="Pre Activation" {...a11yProps(0)} />
+              <Tab label="Post Activation" {...a11yProps(1)} />
+            </Tabs>
+          </div>
+          <TabPanel value={value} index={0}>
+            <LineChart
+              margin={{ top: 50 }}
+              slotProps={{
+                legend: {
+                  itemGap: 20,
+                  padding: -5,
+                  itemMarkHeight: 5,
+                },
+              }}
+              width={700}
+              height={500}
+              series={[
+                {
+                  curve: "linear",
+                  data: firstPreData,
+                  label: "post Activation",
+                },
+                {
+                  curve: "linear",
+                  data: secPreData,
+                  label: "post Activation",
+                },
+              ]}
+              xAxis={[{ scaleType: "point", data: EMGxLabels }]}
+            />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <div
+              style={{
+                position: "absolute",
+                top: "30vh",
+                right: "5vw",
+                width: "12vw",
+                height: "5vh",
+              }}
+            >
+              <select
+                value={tarDate}
+                onChange={ChangeDate}
+                style={{ width: "100%", height: "100%" }}
+              >
+                {dateOptions}
+              </select>
+            </div>
+
+            <LineChart
+              margin={{ top: 50 }}
+              slotProps={{
+                legend: {
+                  itemGap: 20,
+                  padding: -5,
+                  itemMarkHeight: 5,
+                },
+              }}
+              width={700}
+              height={500}
+              series={[
+                {
+                  curve: "linear",
+                  data: firstPostData,
+                  label: "post Activation",
+                  color: "#008dda",
+                },
+              ]}
+              xAxis={[{ scaleType: "point", data: postXLabel }]}
+            />
+          </TabPanel>
         </div>
       </div>
     </div>
