b3f25ef6a5c977f59a408835986e14f89e198861
sky04281 <119783141+sky04281@users.noreply.github.com>
2024-04-18 14:59:20 +0800

    MGC predict with SVR
    requirements update
    sklearn upgrade to 1.4.2

--- server/prediction/predict.py
+++ server/prediction/predict.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import skops.io as sio
+import os
 from datetime import datetime
 from sklearn.preprocessing import MinMaxScaler
 
@@ -8,13 +9,13 @@ def patient_to_dataframe(patient: dict) -> pd.DataFrame:
     # info
     info = patient['info']
     data['age'] = [datetime.now().year - int(info['DOB'][:4])]
-    data['height'] = [info['height']]
-    data['weight'] = [info['weight']]
     for i in ['male', 'female']:
         if info['sex'] == i:
             data[f'sex_{i}'] = [1]
         else:
             data[f'sex_{i}'] = [0]
+    data['height'] = [info['height']]
+    data['weight'] = [info['weight']]
     
     # visit
     visit = patient['visit']
@@ -23,19 +24,12 @@ def patient_to_dataframe(patient: dict) -> pd.DataFrame:
     data['prescription_compesolone'] = [visit['prescription']['compesolone']]
     data['prescription_imuran'] = [visit['prescription']['imuran']]
     for key, value in visit['examination'].items():
-        if key != 'MGFAclassification':
-            data[f'examination_{key}'] = [value]
-    for i in ['IIA', 'IIB', 'IIIA', 'IV']:
-        if visit['examination']['MGFAclassification'] == i:
+        data[f'examination_{key}'] = [value]
+    for i in ['I', 'II', 'IIA', 'IIB', 'III', 'IIIA', 'IIIB', 'IV', 'IVA', 'IVB', 'V']:
+        if visit['MGFAclassification'] == i:
             data[f'MGFAclassification_{i}'] = [1]
         else:
             data[f'MGFAclassification_{i}'] = [0]
-    # thymus
-    for i in [0, 2, 3]:
-        if patient['thymus']['thymusStatus'] == i:
-            data[f'thymusStatus_{i}'] = [1]
-        else:
-            data[f'thymusStatus_{i}'] = [0]
     
     # blood test
     data['bloodTest_ACHR'] = [patient['bloodTest']['ACHR']]
@@ -64,7 +58,7 @@ def patient_to_dataframe(patient: dict) -> pd.DataFrame:
     return df
 
 def predict_MGCscore(x: pd.DataFrame) -> float:
-    SVR_model = sio.load('E:\Coding\專題\MDGSS\server\prediction\MGC\MGCscore_prediction_SVR.skops', trusted=True)
+    SVR_model = sio.load(f'{os.path.dirname(__file__)}/MGC/MGCscore_prediction_SVR.skops', trusted=True)
     MMscaler=MinMaxScaler(feature_range=(0, 1))
     normalize_df=MMscaler.fit_transform(x)
     scaled_x=pd.DataFrame(data=normalize_df)
--- server/routers/prediction.py
+++ server/routers/prediction.py
@@ -5,8 +5,8 @@ from prediction.predict import patient_to_dataframe, predict_MGCscore
 
 router = APIRouter(prefix="/prediction", tags=["prediction"])
 
-@router.post("/data")
-async def get_prediction_data(patient: models.PatientForPredict):
+@router.post("/predict")
+async def predict(patient: models.PatientForPredict):
 	# try:
 		df = patient_to_dataframe(patient.model_dump())	
 		print(predict_MGCscore(df))
