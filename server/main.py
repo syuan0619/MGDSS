f6f9e8640c2283c466aa1df6a173d5b68111d2e5
sky04281 <119783141+sky04281@users.noreply.github.com>
2024-04-20 19:15:53 +0800

    匯出CSV檔 請務必看好說明
    阿因為全部的檔案格式沒有一樣
    所以先用我註解起來的那個測試
    等修好再說

--- server/main.py
+++ server/main.py
@@ -4,29 +4,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
 from mongoDB.connectDB import getAllPatients, addNewPatient, update_patient_info
-from routers import inquiry, account, prediction
+from routers import inquiry, account, prediction, patients
 
 app = FastAPI()
 # routers/inquiry.py
 app.include_router(inquiry.router)
 app.include_router(account.router)
 app.include_router(prediction.router)
+app.include_router(patients.router)
 
-# GET /patients/ -> return all patients
-@app.get("/patients", tags=["patients"])
-async def get_all_patients():
-    return getAllPatients()
-
-# POST /patients/ -> add new patient
-@app.post("/patients", tags=["patients"])
-async def add_new_patient(newPatientInfo: models.Info):
-    try:
-        newPatient = addNewPatient(newPatientInfo.model_dump(by_alias=True))
-        return {"message": "Success add new patient", "newPatient": newPatient}
-    except ValidationError as e:
-        print("error: ", str(e))
-        return JSONResponse(status_code=400, content={"message": "Invalid patient info"})
-    
 @app.post("/updateinfo/{patientId}", tags=["patients"], description="request body: 更新後的info, return: {_id: _id, info:執行後的info}")
 async def update_info(patientId: str, updatedPatientInfo: models.Info):
     try:
---
+++ server/routers/patients.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse, Response
+from pydantic import ValidationError
+from mongoDB import getAllPatients, addNewPatient, all_patients_to_csv
+import models
+import io
+
+router = APIRouter(prefix="/patients", tags=["patient"])
+
+# GET /patients/ -> return all patients
+@router.get("/", tags=["patients"])
+async def get_all_patients():
+    return getAllPatients()
+
+# POST /patients/ -> add new patient
+@router.post("/", tags=["patients"])
+async def add_new_patient(newPatientInfo: models.Info):
+    try:
+        newPatient = addNewPatient(newPatientInfo.model_dump(by_alias=True))
+        return {"message": "Success add new patient", "newPatient": newPatient}
+    except ValidationError as e:
+        print("error: ", str(e))
+        return JSONResponse(status_code=400, content={"message": "Invalid patient info"})
+    
+@router.get("/csv", tags=["patients"], description="下載之後還要右鍵->編輯->另存新檔->編碼選帶有BOM的UTF-8 再用excel開啟才不會亂碼")
+async def get_all_patients_to_csv():
+    records_df = all_patients_to_csv()
+    records_csv = records_df.to_csv(index=False, encoding='utf-8-sig').encode('utf-8-sig')
+    file_stream = io.BytesIO(records_csv)
+    headers = {
+        "Content-Disposition": "attachment; filename=patients.csv",
+        "Content-Type": "text/csv; charset=utf-8-sig",
+    }
+    
+    return Response(file_stream.getvalue(), headers=headers)
