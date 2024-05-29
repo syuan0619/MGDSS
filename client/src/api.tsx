5340698e5ab76f44dfc011721e43efed614dcc3d
sky04281 <119783141+sky04281@users.noreply.github.com>
2024-05-30 02:03:53 +0800

    test build

--- client/src/api.tsx
+++ client/src/api.tsx
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:8000" });
+const api = axios.create({ baseURL: "https://mgdss.onrender.com" });
 export default api;
