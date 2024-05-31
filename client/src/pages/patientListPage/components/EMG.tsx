1a9de6fdfdc4401fa738dbb024b6e68670b0b313
KevinChen <120458845+KevinLaKao@users.noreply.github.com>
2024-05-31 23:54:58 +0800

    護理師上傳EMG

--- client/src/pages/patientListPage/components/EMG.tsx
+++ client/src/pages/patientListPage/components/EMG.tsx
@@ -1,38 +1,36 @@
 import api from "../../../api";
 import { useEffect, useRef, useState } from "react";
 import "../../inquiryPage/table/EMG.css";
+import { noImageType } from "../../../types/Patient";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
 
-const EMG = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
+const EMG = ({
+  selectedDate,
+  handleEMGDialogClose,
+  pid,
+}: {
+  selectedDate: string;
+  handleEMGDialogClose: () => void;
+  pid: string;
+}) => {
+  const [timeToSend, setTimeToSend] = useState<boolean>();
   const [previewUrl, setPreviewUrl] = useState<string>();
-  const [uploadedFile, setUploadedFile] = useState<File>();
   const [recognizedResult, setRecognizedResult] = useState<string>("");
   const [modifiedResult, setModifiedResult] = useState<string>("");
-  console.log(modifiedResult);
+  const [resultHeader, setResultHeader] = useState<noImageType>();
+  const [resultBody, setResultBody] = useState<Blob>();
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const fileSelectedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files && event.target.files[0];
     if (file) {
-      setUploadedFile(file);
-    }
-  };
-
-  const buttonHandler = () => {
-    inputRef.current?.click();
-  };
-
-  const getRecognized = async (formData: FormData) => {
-    return await api.post("/inquiry/recognize", formData, {
-      responseType: "blob",
-    });
-  };
-
-  useEffect(() => {
-    if (uploadedFile) {
       const formData = new FormData();
-      const image = uploadedFile;
+      const image = file;
       formData.append("file", image);
       getRecognized(formData).then((res) => {
+        console.log(res);
+        setResultBody(res.data);
+
         const imageBlob = URL.createObjectURL(res.data);
         setPreviewUrl(imageBlob);
 
@@ -45,11 +43,11 @@ const EMG = () => {
             postActivation: string[];
           }) => {
             recognizedString += `測試部位: ${result.musclePart}\n`;
-            recognizedString += `\nPreActivation:\n\n       Amp\n1-5:  ${
+            recognizedString += `\nPreActivation:\n\n         Amp\n1-5:  ${
               result.preActivation[0].split(" ")[0]
             }\n`;
             if (result.postActivation.length > 0) {
-              recognizedString += `\nPostActivation:\n\n       Amp\n`;
+              recognizedString += `\nPostActivation:\n\n         Amp\n`;
               result.postActivation.forEach((postAct, idx) => {
                 if (idx % 3 === 0) {
                   recognizedString += `1-5:  ${postAct.split(" ")[0]}\n`;
@@ -64,16 +62,232 @@ const EMG = () => {
         setModifiedResult(recognizedString);
       });
     }
-  }, [uploadedFile]);
+  };
+
+  const uploadHandler = () => {
+    inputRef.current?.click();
+  };
+
+  const getRecognized = async (formData: FormData) => {
+    return await api.post("/inquiry/recognize", formData, {
+      responseType: "blob",
+    });
+  };
+
+  const noSpaceNewLineResult: string[] = [];
+  const handleModifiedResult = () => {
+    const modified = modifiedResult;
+    let word = "";
+    for (let i = 0; i < modifiedResult.length; i++) {
+      if (!(modified[i] == "\n" || modified[i] == "\r" || modified[i] == " ")) {
+        word += modified[i];
+      } else if (word.length > 0) {
+        noSpaceNewLineResult.push(word);
+        word = "";
+      }
+    }
+  };
+
+  const modifiedResultToEMG = () => {
+    const sendResult: noImageType = {
+      testDate: selectedDate,
+      nasalis: {
+        preActivation: 0,
+        postActivation: [],
+      },
+      abd: {
+        preActivation: 0,
+        postActivation: [],
+      },
+      trapezius: {
+        preActivation: 0,
+        postActivation: [],
+      },
+    };
+
+    noSpaceNewLineResult.map((eachWord, index) => {
+      typeof eachWord === "string" ? toPureString(eachWord) : null;
+      if (eachWord.toLowerCase() === "nasalis") {
+        if (
+          typeof noSpaceNewLineResult[index + 1] === "string" &&
+          toPureString(noSpaceNewLineResult[index + 1])
+            .toLowerCase()
+            .includes("preactivation")
+        ) {
+          sendResult.nasalis.preActivation = toPureNumber(
+            noSpaceNewLineResult[index + 4]
+          );
+        }
+        if (
+          typeof noSpaceNewLineResult[index + 5] === "string" &&
+          toPureString(noSpaceNewLineResult[index + 5])
+            .toLowerCase()
+            .includes("postactivation")
+        ) {
+          sendResult.nasalis.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 8])
+          );
+          sendResult.nasalis.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 10])
+          );
+          sendResult.nasalis.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 12])
+          );
+          sendResult.nasalis.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 14])
+          );
+        }
+      }
+      if (eachWord.toLowerCase() === "abd") {
+        if (
+          typeof noSpaceNewLineResult[index + 1] === "string" &&
+          toPureString(noSpaceNewLineResult[index + 1])
+            .toLowerCase()
+            .includes("preactivation")
+        ) {
+          sendResult.abd.preActivation = toPureNumber(
+            noSpaceNewLineResult[index + 4]
+          );
+        }
+        if (
+          typeof noSpaceNewLineResult[index + 5] === "string" &&
+          toPureString(noSpaceNewLineResult[index + 5])
+            .toLowerCase()
+            .includes("postactivation")
+        ) {
+          sendResult.abd.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 8])
+          );
+          sendResult.abd.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 10])
+          );
+          sendResult.abd.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 12])
+          );
+          sendResult.abd.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 14])
+          );
+        }
+      }
+      if (eachWord.toLowerCase() === "trapezius") {
+        if (
+          typeof noSpaceNewLineResult[index + 1] === "string" &&
+          toPureString(noSpaceNewLineResult[index + 1])
+            .toLowerCase()
+            .includes("preactivation")
+        ) {
+          sendResult.trapezius.preActivation = toPureNumber(
+            noSpaceNewLineResult[index + 4]
+          );
+        }
+        if (
+          typeof noSpaceNewLineResult[index + 5] === "string" &&
+          toPureString(noSpaceNewLineResult[index + 5])
+            .toLowerCase()
+            .includes("postactivation")
+        ) {
+          sendResult.trapezius.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 8])
+          );
+          sendResult.trapezius.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 10])
+          );
+          sendResult.trapezius.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 12])
+          );
+          sendResult.trapezius.postActivation.push(
+            toPureNumber(noSpaceNewLineResult[index + 14])
+          );
+        }
+      }
+    });
+    setResultHeader(sendResult);
+  };
+
+  const toPureNumber = (str: string | undefined): number => {
+    if (str !== undefined) {
+      let newStr = "";
+      for (let i = 0; i < str.length; i++) {
+        if (!(str[i] === "%")) {
+          newStr += str[i];
+        }
+      }
+      return Number(newStr);
+    }
+    return 0;
+  };
+
+  const toPureString = (str: string | undefined): string => {
+    if (str !== undefined) {
+      let newStr = "";
+      for (let i = 0; i < str.length; i++) {
+        if (!(str[i] === "%" || str[i] === ":")) {
+          newStr += str[i];
+        }
+      }
+      return newStr;
+    }
+    return "";
+  };
+
+  const sendResultApiPending = async () => {
+    console.log("sendResultApiPending");
+    const formdata = new FormData();
+    formdata.append("file", resultBody!);
+    if (resultHeader && formdata) {
+      console.log(resultHeader);
+      await api
+        .post(`/inquiry/${pid}/EMG`, formdata, {
+          headers: {
+            table: JSON.stringify(resultHeader),
+            "Access-Control-Expose-Headers": "table",
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  };
+
+  const sendResult = () => {
+    if (modifiedResult) {
+      handleModifiedResult();
+      modifiedResultToEMG();
+      setTimeToSend(true);
+      handleEMGDialogClose();
+    } else {
+      alert("請先上傳圖片");
+    }
+  };
+
+  useEffect(() => {
+    sendResultApiPending();
+  }, [timeToSend]);
 
   return (
     <div className="inquiry-table-EMG-all">
       <div className="inquiry-table-EMG-head-content">
         <div className="inquiry-table-EMG-head">
-          <div>
+          <div className="inquiry-table-EMG-return">
+            <button
+              className="EMG-backToRight"
+              onClick={() => {
+                setPreviewUrl("");
+                setRecognizedResult("");
+                setModifiedResult("");
+                handleEMGDialogClose();
+              }}
+            >
+              <IoIosArrowDropleftCircle />
+            </button>
+          </div>
+          <div className="inquiry-table-EMG-head-title">
             <p>電生理訊號</p>
           </div>
-          <div></div>
+          <div className="inquiry-table-EMG-content-row-sum"></div>
         </div>
 
         <div className="inquiry-table-EMG-content">
@@ -86,9 +300,9 @@ const EMG = () => {
                 ref={inputRef}
               />
               {previewUrl ? (
-                <img src={previewUrl} alt="Preview" />
+                <img src={previewUrl} alt="Preview" onClick={uploadHandler} />
               ) : (
-                <button onClick={buttonHandler}>檔案上傳/預覽</button>
+                <button onClick={uploadHandler}>檔案上傳/預覽</button>
               )}
             </div>
           </div>
@@ -97,7 +311,7 @@ const EMG = () => {
               <h3 className="h3">辨識結果 : </h3>
               <textarea
                 id="result"
-                defaultValue={recognizedResult ? recognizedResult : ""}
+                value={recognizedResult ? recognizedResult : ""}
                 readOnly
               />
             </div>
@@ -105,7 +319,7 @@ const EMG = () => {
               <h3 className="h3">手動修正 :</h3>
               <textarea
                 id="modifyCon"
-                defaultValue={modifiedResult ? modifiedResult : ""}
+                value={modifiedResult ? modifiedResult : ""}
                 onChange={(e) => {
                   setModifiedResult(e.target.value);
                 }}
@@ -115,9 +329,7 @@ const EMG = () => {
               <button
                 id="submitButton"
                 onClick={() => {
-                  if (confirm("確定送出結果嗎?")) {
-                    console.log("送出結果：", recognizedResult);
-                  }
+                  sendResult();
                 }}
               >
                 將結果加入病歷
@@ -131,81 +343,3 @@ const EMG = () => {
 };
 
 export default EMG;
-
-// 圖片上傳後預覽
-// const reader = new FileReader();
-// reader.readAsDataURL(file);
-// reader.onloadend = () => {
-//   if (typeof reader.result === "string") {
-//     setPreviewUrl(reader.result);
-//     setUploadedFile(file);
-//   }
-// };
-
-// 圖片回傳1
-// console.log(res.data);
-// const binaryDataBuffer = res.data;
-// const bufferArray = new Uint8Array(binaryDataBuffer).buffer;
-// const blob = new Blob([bufferArray], {
-//   type: "image/png",
-// });
-// const reader = new FileReader();
-// reader.readAsDataURL(blob);
-// reader.onload = () => {
-//   console.log(reader.result);
-// };
-// reader.onerror = () => {
-//   console.log(reader.error);
-// };
-
-// 圖片回傳2
-// console.log(typeof res.data);
-// const textEncoder = new TextEncoder();
-// const uint8Array = textEncoder.encode(res.data);
-// console.log(typeof uint8Array);
-// const blob = new Blob([uint8Array]);
-// console.log(blob);
-// const imageBlob = URL.createObjectURL(blob);
-
-// 舊版輸出
-// recognizedString += `測試部位: ${result.musclePart}\n`;
-// recognizedString += "測試結果(Amp, Area): \n";
-// recognizedString += `\nPreActivation:\n`;
-// result.preActivation.forEach((preAct, idx) => {
-//   if (idx % 3 === 0) {
-//     recognizedString += `1-5: ${preAct}\n`;
-//   } else if (idx % 3 === 1) {
-//     recognizedString += `#1: ${preAct}\n`;
-//   } else if (idx % 3 === 2) {
-//     recognizedString += `#5: ${preAct}\n\n`;
-//   }
-// });
-// if (result.postActivation.length > 0) {
-//   recognizedString += `\nPostActivation:\n`;
-//   result.postActivation.forEach((postAct, idx) => {
-//     if (idx % 3 === 0) {
-//       recognizedString += `1-5: ${postAct}\n`;
-//     } else if (idx % 3 === 1) {
-//       recognizedString += `#1: ${postAct}\n`;
-//     } else if (idx % 3 === 2) {
-//       recognizedString += `#5: ${postAct}\n\n`;
-//     }
-//   });
-// } else {
-//   recognizedString += "\n";
-// }
-
-// setUploadedFile(res.data);
-// setIsUploaded(true);
-// console.log(res.data);
-// const results = res.data as [];
-// let recognizedString = "";
-// results.forEach(
-//   (result: { musclePart: string; preActivation: string[] }) => {
-//     recognizedString += `測試部位: ${result.musclePart}\n`;
-//     recognizedString += "測試結果(Amp, Area): \n";
-//     recognizedString += `1~5: (${result.preActivation[0]})\n`;
-//     recognizedString += `#1: (${result.preActivation[1]})\n`;
-//     recognizedString += `#5: (${result.preActivation[2]})\n\n`;
-//   }
-// );
